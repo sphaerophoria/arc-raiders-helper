27881eb979262792bb4823aefba0e688e3c0f434
Mick Sayson <mick@sayson.com>
2025-11-16 14:42:18 -0800

    :)

--- metaforge-dump/preprocess.py
+++ metaforge-dump/preprocess.py
@@ -3,16 +3,47 @@
 from pathlib import Path
 import json
 
+def fixSalvageInfo(info):
+    ret = []
+    if info is None:
+        return None
+
+    for key, value in info.items():
+        ret.append({
+            "id": key,
+            "quantity": value
+        })
+    return ret
+
+def checkSalvageInfo(info, all_ids):
+    for mapping in info.values():
+        if mapping is None:
+            continue
+
+        for key in mapping.keys():
+            if key not in all_ids:
+                raise RuntimeError(f"{key} is not a valid id")
+
 def main():
     metaforge_files = Path("downloaded")
+    with open("extradata.json") as f:
+        extradata = json.load(f)
 
+    salvage_info = {}
+    for item in extradata:
+        salvage_info[item["id"]] = item["scraps_into"]
+
+    print(salvage_info["candle-holder"])
     out = []
+
+    all_ids = set()
 
     for p in metaforge_files.glob("*.json"):
         with p.open("r") as f:
             data = json.load(f)
 
         for item in data["data"]:
+            all_ids.add(item["id"])
             components = []
 
             for component in item["components"]:
@@ -40,7 +71,10 @@ def main():
                 "value": item["value"],
                 "components": components,
                 "recycle_components": recycle_components,
+                "salvage_components": fixSalvageInfo(salvage_info[item["id"]])
             })
+
+    checkSalvageInfo(salvage_info, all_ids)
 
     with open("preprocessed.json", "w") as f:
         json.dump(out, f, indent=2)
